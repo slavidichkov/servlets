5d5eba7252cb66758b442ac695a88496106cd201
slavidichkov <dichkof@gmail.com>
2016-04-06 09:51:47 +0300

    page presenter

---
+++ pagepresenter/src/main/java/com/clouway/http/SecondPage.java
@@ -0,0 +1,20 @@
+package com.clouway.http;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Slavi Dichkov (slavidichkof@gmail.com)
+ */
+public class SecondPage extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("page","second");
+        RequestDispatcher dispatcher = req.getRequestDispatcher("router");
+        dispatcher.forward(req, resp);
+    }
+}
