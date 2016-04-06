0e7e3a985aa325f42232aee6c1995806ecc21a46
slavidichkov <dichkof@gmail.com>
2016-04-06 09:43:38 +0300

    link access counter

---
+++ linkaccesscounter/src/main/java/com/clouway/server/Jetty.java
@@ -0,0 +1,28 @@
+package com.clouway.server;
+
+import com.clouway.http.HttpServletContextListener;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+
+
+public class Jetty {
+
+    private final Server server;
+
+    public Jetty(int port) {
+        this.server = new Server(port);
+    }
+
+    public void start() {
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        context.addEventListener(new HttpServletContextListener());
+        server.setHandler(context);
+        try {
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
