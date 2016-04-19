055552c057902422d802beafb145b04a878cfbce
slavidichkov <dichkof@gmail.com>
2016-04-19 13:46:19 +0300

    use dummy table to test DatabaseHelper

--- bank/src/main/test/com/clouway/adapter/persistence/sql/DatabaseHelperTest.java
+++ bank/src/main/test/com/clouway/adapter/persistence/sql/DatabaseHelperTest.java
@@ -1,18 +1,15 @@
 package com.clouway.adapter.persistence.sql;
 
-import com.clouway.adapter.persistence.sql.util.DatabaseCleaner;
-import com.clouway.adapter.persistence.sql.util.FakeUserRowFetcher;
+import com.clouway.adapter.persistence.sql.util.TestUser;
 import com.clouway.adapter.persistence.sql.util.TestingDatasource;
 import com.clouway.core.User;
 import com.google.common.base.Optional;
-import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
-import org.jmock.auto.Mock;
-import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -22,43 +19,45 @@ import static org.junit.Assert.assertThat;
  * @author Slavi Dichkov (slavidichkof@gmail.com)
  */
 public class DatabaseHelperTest {
-  private MysqlConnectionPoolDataSource dataSource;
-
-  @Rule
-  public JUnitRuleMockery context = new JUnitRuleMockery();
-
-  @Mock
-  RowFetcher<User> rowFetcher;
-
   private DatabaseHelper databaseHelper;
+  private DataSource dataSource;
 
   @Before
-  public void setUp() {
-    DataSource dataSource = new TestingDatasource().get();
-    new DatabaseCleaner(dataSource, "users", "sessions", "accounts").cleanUp();
+  public void setUp() throws SQLException {
+    dataSource = new TestingDatasource().get();
+    dataSource.getConnection().createStatement().execute("CREATE TABLE helperTests(name VARCHAR(40),city VARCHAR(30))");
     databaseHelper = new DatabaseHelper(dataSource);
   }
 
   @Test
   public void insertingObject() throws SQLException {
-    User user = new User("ivan", "ivan123", "ivan@abv.bg", "ivan123", "sliven", 23);
-
-    databaseHelper.executeUpdate("insert into users(userName,nickName,email,password,city,age) values(?,?,?,?,?,?)", user.name, user.nickName, user.email, user.password, user.city, user.age);
+    databaseHelper.executeUpdate("insert into helperTests(name,city) values(?,?)","ivan","sliven" );
   }
 
   @Test
   public void executeQueryWithExistingObject() throws SQLException {
-    User user = new User("ivan", "ivan123", "ivan@abv.bg", "ivan123", "sliven", 23);
+    databaseHelper.executeUpdate("insert into helperTests(name,city) values(?,?)","ivan","sliven" );
 
-    databaseHelper.executeUpdate("insert into users(userName,nickName,email,password,city,age) values(?,?,?,?,?,?)", user.name, user.nickName, user.email, user.password, user.city, user.age);
-
-    Optional<User> optUser = databaseHelper.fetchOne("select * from users where userName=?", new FakeUserRowFetcher(), "ivan");
+    Optional<TestUser> optUser = databaseHelper.fetchOne("select * from helperTests where name=?", new RowFetcher<TestUser>() {
+      public TestUser build(ResultSet resultSet) throws SQLException {
+        return new TestUser(resultSet.getString("name"),resultSet.getString("city"));
+      }
+    }, "ivan");
     assertThat(optUser.isPresent(), is(true));
   }
 
   @Test
   public void executeQueryWithNotExistingObject() throws SQLException {
-    Optional<User> optUser = databaseHelper.fetchOne("select * from users where userName=?", new FakeUserRowFetcher(), "ivan");
+    Optional<TestUser> optUser = databaseHelper.fetchOne("select * from helperTests where name=?", new RowFetcher<TestUser>() {
+      public TestUser build(ResultSet resultSet) throws SQLException {
+        return new TestUser(resultSet.getString("name"),resultSet.getString("city"));
+      }
+    }, "ivan");
     assertThat(optUser.isPresent(), is(false));
+  }
+
+  @After
+  public void tearDown() throws SQLException {
+    dataSource.getConnection().createStatement().execute("DROP TABLE helperTests");
   }
 }
